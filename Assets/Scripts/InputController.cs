e3be8cab3f5865743db5c56f5439f119d6e2134b
ambiguousname <tyly04@gmail.com>
2022-04-03 17:02:07 -0700

    Added text input script

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour
+{
+    Text title;
+    InputField input;
+
+    Action<string> activeCallback;
+
+    private void Start()
+    {
+        title = gameObject.FindChildWithName("Text").GetComponent<Text>();
+        input = gameObject.FindChildWithName("TextInput").GetComponent<InputField>();
+    }
+
+    public void CreateDialogue(string titleText, Action<string> callback) {
+        gameObject.SetActive(true);
+        title.text = titleText;
+        activeCallback = callback;
+    }
+
+    // Start is called before the first frame update
+    public void Submit(string text) {
+        if (input.text.Length > 0)
+        {
+            gameObject.SetActive(false);
+            activeCallback(text);
+        }
+    }
+}
