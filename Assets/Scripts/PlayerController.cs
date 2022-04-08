9e71dd064269cafc9d77e228ed56af751e6783f3
ambiguousname <tyly04@gmail.com>
2022-04-07 18:45:56 -0700

    Got movement code with CharacterController 90% working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     private DialogueCreator creator;
     private GameObject _camera;
     private float targetPitch;
+    private Vector2 _move;
+    Vector3 shipPos;
+    Vector3 currRot;
+    GameObject ship;
+
     private void Start()
     {
         _camera = gameObject.FindChildWithTag("MainCamera");
@@ -17,14 +22,30 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         targetPitch = _camera.transform.localEulerAngles.x;
+        ship = GameObject.Find("ShipPrefab");
+        shipPos = ship.transform.position;
+        currRot = ship.transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //GetComponent<ShipSticker>().MoveUpdate(this.transform.position - currPos);
+        //this.transform.localPosition += (_camera.transform.forward * _move.y + _camera.transform.right * _move.x) * Time.deltaTime;
 
     }
-    
+
+    private void LateUpdate()
+    {
+        Vector3 dist = ship.transform.position - shipPos;
+        Vector3 rotDist = ship.transform.eulerAngles - currRot;
+        Vector3 playerPos = Helper.RotateAroundPivot(this.transform.position + dist, ship.transform.position, rotDist);
+        GetComponent<CharacterController>().Move(playerPos - this.transform.position);
+        this.transform.rotation = Quaternion.Euler(ship.transform.eulerAngles);
+        shipPos = ship.transform.position;
+        currRot = ship.transform.eulerAngles;
+    }
+
     // From StarterAssets and FirstPersonController.cs
     private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
     {
@@ -39,6 +60,11 @@ public class PlayerController : MonoBehaviour
         targetPitch = ClampAngle(targetPitch, -90.0f, 90.0f);
         Vector3 fullRot = new Vector3(targetPitch, _camera.transform.localEulerAngles.y + (toRot.x * Time.deltaTime * rotationSpeed), _camera.transform.localEulerAngles.z);
         _camera.transform.localRotation = Quaternion.Euler(fullRot);
+    }
+
+    void OnMove(InputValue value) {
+        var toMove = value.Get<Vector2>();
+        _move = toMove;
     }
 
     void OnOne(InputValue value)
